cc1a3e2a3dd0e9cda1637781d53b90a428f1de27
TheRealPaulyglot <PaulESanchezC@outlook.com>
2022-04-29 16:27:14 -0400

    Models

--- AsvesdoService/Models/PaymentModels/PaymentBase.cs
+++ AsvesdoService/Models/PaymentModels/PaymentBase.cs
@@ -1,6 +1,23 @@
-﻿namespace Models.PaymentModels;
+﻿using System.ComponentModel.DataAnnotations;
+using Models.CustomerModels;
+using Models.OrderModels;
+using Models.TaxModels;
+
+namespace Models.PaymentModels;
 
 public class PaymentBase
 {
+    #region Public Properties
 
+    [Key]
+    public string PaymentId { get; set; } = Guid.NewGuid().ToString();
+
+    public string PaymentMethod { get; set; }
+    public DateTime PaymentDate { get; set; } = DateTime.Now;
+    public CustomerBase Customer { get; set; }
+    public OrderBase Order { get; set; }
+    public double TotalAmount { get; set; }
+    public List<TaxBase> Taxes { get; set; }
+
+    #endregion
 }
---
+++ AsvesdoService/Models/TaxModels/TaxBase.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models.TaxModels;
+
+public class TaxBase
+{
+    #region Public properties
+
+    [Key] public string TaxId { get; set; }
+    public double TaxPercentage { get; set; }
+    public string TaxDescription { get; set; }
+    public string TaxAcronym { get; set; }
+
+    #endregion
+
+    #region Internal Properties
+    public DateTime DateCreated { get; set; } = DateTime.Now;
+    public bool IsActive { get; set; }
+    #endregion
+}
